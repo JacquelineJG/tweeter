5970b2f0aa1b5a63fbf8546dbc977a9f5e38b071
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-10-08 22:18:27 +0000

    clean up of code and commenting for clarity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,15 +1,13 @@
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
 const renderTweets = function(tweets) {
   const tweetContainer = $('.tweet-container').html('')
-  console.log(tweets);
-  console.log(tweetContainer);
   // loops through tweets
   for (tweet of tweets) {
     tweetContainer.prepend(createTweetElement(tweet));
   }
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
 }
-
+// creates relative for when tweet is created
 function relativeTime(date) {
   let seconds = Math.floor((new Date() - date) / 1000);
   let interval = seconds / 31536000;
@@ -35,39 +33,38 @@ function relativeTime(date) {
   }
   return Math.floor(seconds) + " seconds";
 }
-
-
+// escapes from malicious text insterted into new tweet form
 const escape =  function(str) {
-  console.log('str', str)
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
-
+// function for creation of new tweet
 const createTweetElement = function(tweet) {
   const safeHTML = `<p>${escape(tweet.content.text)}</p>`;
-let $tweet = $(`<article>
-<div class='tweet-top'>
-<div>
-  <img src='${tweet.user.avatars}' width="60" height="60">
-</div>
-<div class='name'>${tweet.user.name}</div>
-<div class='handle'>${tweet.user.handle}</div>
-</div>
-<div class='tweet-middle'>
-<p>${safeHTML}</p>
-</div>
-<div class='tweet-bottom'>
-<div class='time'>
-${relativeTime(tweet.created_at)}
-</div>
-<div class='symbols'>
-<i class="fa fa-flag" style="font-size:20px;color:#6e50f2"></i>
-<i class="fa fa-retweet" style="font-size:20px;color:#6e50f2"></i>
-<i class="fa fa-heart" style="font-size:20px;color:#6e50f2"></i>
-</div>
-</div>
-</article>`);
+  let $tweet = $(
+  `<article>
+    <div class='tweet-top'>
+      <div>
+        <img src='${tweet.user.avatars}' width="60" height="60">
+      </div>
+      <div class='name'>${tweet.user.name}</div>
+      <div class='handle'>${tweet.user.handle}</div>
+    </div>
+    <div class='tweet-middle'>
+      <p><b>${safeHTML}</b></p>
+    </div>
+    <div class='tweet-bottom'>
+      <div class='time'>
+        ${relativeTime(tweet.created_at)}
+      </div>
+      <div class='symbols'>
+        <i class="fa fa-flag" style="font-size:20px;color:#6e50f2"></i>
+        <i class="fa fa-retweet" style="font-size:20px;color:#6e50f2"></i>
+        <i class="fa fa-heart" style="font-size:20px;color:#6e50f2"></i>
+      </div>
+    </div>
+  </article>`);
 
 return $tweet;
 }  
@@ -76,15 +73,16 @@ $(document).ready(function() {
   console.log('Ready!')
   $('.tweet-form').submit(function(event) {
     event.preventDefault()
+    //if all checks clear for a tweet, make a post request, then load the tweets same page and reset the counter and text box
     if($('.new-tweet .counter').val() < 140 && $('.new-tweet .counter').val() > 0) {
-    $.ajax('/tweets', 
-    { method: 'POST', 
-      data: $(this).serialize()
-    }).then(function() {
-      loadTweets()
-      $('#tweet-text').val('')
-      $('.new-tweet .counter').val(140)
-    })
+      $.ajax('/tweets', 
+      { method: 'POST', 
+        data: $(this).serialize()
+      }).then(function() {
+        loadTweets()
+        $('#tweet-text').val('')
+        $('.new-tweet .counter').val(140)
+      })
     } else if ($('.new-tweet .counter').val() == 140) {
       $('.error').text('Error: Tweet cannot be empty').hide().slideDown(2000);
       setTimeout(function() { 
@@ -98,8 +96,7 @@ $(document).ready(function() {
         $(".error").empty();
       }, 3000);
     }
-  
-  }),
+  })
   
   $(loadTweets = function() {
       $.ajax('/tweets', { method: 'GET' })
